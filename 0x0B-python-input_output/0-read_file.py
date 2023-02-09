91acb51113b276a78b8b43a7aa9adbfd0fc2cc0c
Agyemang Gerron <agyemanggerron@gmail.com>
2023-02-09 04:34:53 +0000

    File reading - Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defining a text file-reading method/function."""
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
